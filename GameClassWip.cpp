456013e8e88ce40c67fae9810d40270d470bdc13
LyreLilith <103064593+LyreLilith@users.noreply.github.com>
2023-04-22 00:19:06 -0700

    Fixed framrate issue

--- GameClassWip.cpp
+++ GameClassWip.cpp
@@ -4,6 +4,7 @@ GameWip::GameWip(int width, int height, std::string title)
 {
 	data->window.create(sf::VideoMode(width, height), title,
 		sf::Style::Close | sf::Style::Titlebar);
+	data->window.setFramerateLimit(60);
 	data->machine.AddState(StateRef(new SplashState(this->data)));
 	this->Run();
 }
